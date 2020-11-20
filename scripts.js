edbb3ccbac2649229e24be81004544993af8a679
Priyanka26-lab <73358310+Priyanka26-lab@users.noreply.github.com>
2020-11-20 20:57:06 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,79 @@
+
+
+
+function add(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1+number2
+    alert(`The sum of first and second number is:${result}`)
+}
+function add(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1+number2
+    alert(`The sum of first and second number is:${result}`)
+}
+function sub(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1-number2
+    alert(`The Diffrence of first and second number is:${result}`)
+}
+function mul(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1*number2
+    alert(`The product of first and second number is:${result}`)
+}
+function div(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1/number2
+    alert(`The quotient of first and second number is:${result}`)
+}
+function mod(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1%number2
+    alert(`The modules of first and second number is:${result}`)
+}
+function pow(){
+    var number1=prompt("Enter the first number")
+    var number2=prompt("Enter the second number")
+    number1= parseInt(number1)
+    number2= parseInt(number2)
+    var result=number1^number2
+    alert(`The power of first and second number is:${result}`)
+}
+function sqr(){
+    var number1=prompt("Enter the  number")
+    number1= parseInt(number1)
+    var result=number1*number1
+    alert(`The squareroot of the number:${result}`)
+}
+function cub(){
+    var number1=prompt("Enter the  number")
+    number1= parseInt(number1)
+    var result=number1*number1*number1
+    alert(`The cube of the number is:${result}`)
+}
+// add()
+// sub()
+// mul()
+// div()
+// mod()
+// pow()
+// sqr()
+// cub()
